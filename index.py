0ad8ed4eead12c0d9fd2a99faefbaaad080a8ad7
OmarShiriniani <omar@unioay.com>
2020-07-30 14:55:46 +0400

    remake idea

--- index.py
+++ index.py
@@ -4,39 +4,34 @@ import random
 import xlsxwriter
 import statistics 
 from random import randint, uniform
-conn = sqlite3.connect('PersonalDB.db')
+conn = sqlite3.connect('condDB.db')
 mycursor = conn.cursor()
+
 records = []
 stat = []
-personWorkingTimes = []
-alp = ["ა","ბ","გ","დ","ე","ვ","ზ","თ","ი","კ","ლ","მ","ნ","ო","პ","ჟ","რ","ს","ტ","უ","ფ","ქ","ღ","ყ","შ","ჩ","ც","ძ","წ","ჭ","ხ","ჯ","ჰ"]
 
-def getRandomName():
+personWorkingTimes = []
+alp = ["A","B","C","D","E","F","G","H","I","K","L","M","N","O","P","Q","R","S","T","V","X","Y","Z"]
+
+def getModel():
     chars = []
     for j in range(0, 5):
-        index = randint(0, 32)
+        index = randint(0, 22)
         chars.append(alp[index])
     return("".join(chars))
 
-def getRandomLastName():
-    chars = []
-    for j in range(0, 10):
-        index = randint(0, 32)
-        chars.append(alp[index])
-    return("".join(chars))
-
-def getRandomAge():
-    index = randint(20, 60)
+def getPrice():
+    index = randint(500, 5000)
     return(str(index))
 
-def getRandomPersonalN():
+def getCondIdNumber():
     chars = []
     for j in range(0, 11):
         index = randint(0, 9)
         chars.append(str(index))
     return("".join(chars))
 
-def getRandomWorkTime():
+def getWorkTime():
     sum = 0
     stat = []
     for j in range(0, 30):
@@ -47,46 +42,44 @@ def getRandomWorkTime():
     return("{:.1f}".format(sum))
 
 for i in range(0, 100): 
-    person = {"name": getRandomName(), "lastName": getRandomLastName(), "age": getRandomAge(), "personalN": getRandomPersonalN(), "workTime": getRandomWorkTime()}
-    records.append(person)
-    sql = "INSERT INTO person (name, lastName, age, personalN, workTime)  VALUES ('"+person["name"]+"', '"+person["lastName"]+"', '"+person["age"]+"', '"+person["personalN"]+"', '"+person["workTime"]+"')"
+    cond = {"model": getModel(), "price": getPrice(), "condID": getCondIdNumber(), "workTime": getWorkTime()}
+    records.append(cond)
+    sql = "INSERT INTO cond (model,  price, condID, workTime)  VALUES ('"+cond["model"]+"', '"+cond["price"]+"', '"+cond["condID"]+"', '"+cond["workTime"]+"')"
     mycursor.execute(sql)
 conn.commit()
 
 # Step2
 
-def getAverage():
+def getSoldCond():
     sum = 0
     for i in records:
-        sum += int(i['age'])
+        sum += int(i['price'])
     return(sum / len(records))
-average = getAverage()
+sodlCond = getSoldCond()
 
-workbook = xlsxwriter.Workbook('personal.xlsx')
+workbook = xlsxwriter.Workbook('cond.xlsx')
 worksheet = workbook.add_worksheet()
 worksheet2 = workbook.add_worksheet()
 row = 0
 row2 = 0
 col = 0
-worksheet.write(row, col,     "Name")
-worksheet.write(row, col + 1, "Last Name")
-worksheet.write(row, col + 2, "Age")
-worksheet.write(row, col + 3, "Personal Number")
-worksheet.write(row, col + 4, "Work Time")
-worksheet.write(row, col + 5, "Average Work Time")
+worksheet.write(row, col,     "Model")
+worksheet.write(row, col + 1, "Price")
+worksheet.write(row, col + 2, "Cond ID Number")
+worksheet.write(row, col + 3, "Work Time")
+worksheet.write(row, col + 4, "Sold Cond")
 worksheet2.write(row, col, "Statistics")
 
 row += 1
 row2 += 1
 
 for i in records:
-    if int(i['age']) > average :
-        worksheet.write(row, col,     str(i["name"]))
-        worksheet.write(row, col + 1, str(i["lastName"]))
-        worksheet.write(row, col + 2, str(i["age"]))
-        worksheet.write(row, col + 3, str(i["personalN"]))
-        worksheet.write(row, col + 4, str(i["workTime"]))
-        worksheet.write(row, col + 5, round(float(i["workTime"])))
+    if int(i['price']) > sodlCond :
+        worksheet.write(row, col,     str(i["model"]))
+        worksheet.write(row, col + 1, str(i["price"]))
+        worksheet.write(row, col + 2, str(i["condID"]))
+        worksheet.write(row, col + 3, str(i["workTime"]))
+        worksheet.write(row, col + 4, round(float(i["workTime"])))
         row += 1
 
 for j in personWorkingTimes:
@@ -94,6 +87,6 @@ for j in personWorkingTimes:
     row2 += 1
 
 chart = workbook.add_chart({'type': 'line'})
-chart.add_series({'values': '=Sheet1!$F$1:$F$'+str(row-1)})
+chart.add_series({'values': '=Sheet1!$E$1:$E$'+str(row-1)})
 worksheet.insert_chart('H5', chart)
 workbook.close()
